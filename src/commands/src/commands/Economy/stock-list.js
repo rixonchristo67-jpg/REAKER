cf26645b352db6733229a4a432d1e5b129e932c9
rixonchristo67-jpg <rixonchristo67@gmail.com>
2026-06-20 16:01:40 +0530

    Create stock-list.js

---
+++ src/commands/src/commands/Economy/stock-list.js
@@ -0,0 +1,25 @@
+import { SlashCommandBuilder } from 'discord.js';
+import fs from 'fs';
+
+export default {
+    data: new SlashCommandBuilder()
+        .setName('stocklist')
+        .setDescription('View stock'),
+
+    async execute(interaction) {
+        const stock = JSON.parse(
+            fs.readFileSync('./src/stock/stock.json', 'utf8')
+        );
+
+        let message = '📦 Current Stock\n\n';
+
+        for (const product in stock) {
+            message += `${product}: ${stock[product].length}\n`;
+        }
+
+        await interaction.reply({
+            content: message,
+            ephemeral: true
+        });
+    }
+};
